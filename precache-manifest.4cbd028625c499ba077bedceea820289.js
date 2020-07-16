6842d8827d59b87870e026d0c3ef4c0406b0f124
Jan Žitník <mail@janzitnik.com>
2020-07-16 16:31:05 +0200

    Updates

---
+++ precache-manifest.4cbd028625c499ba077bedceea820289.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb10d6bfc46e8a386658cb09af633398",
+    "url": "/pwa-sample/index.html"
+  },
+  {
+    "revision": "48de2f1e68b25703472d",
+    "url": "/pwa-sample/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1d76589800db00af5d74",
+    "url": "/pwa-sample/static/js/2.d30010a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pwa-sample/static/js/2.d30010a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48de2f1e68b25703472d",
+    "url": "/pwa-sample/static/js/main.affe3ce3.chunk.js"
+  },
+  {
+    "revision": "14f5016e453549d6c6af",
+    "url": "/pwa-sample/static/js/runtime-main.df716211.js"
+  }
+]);
